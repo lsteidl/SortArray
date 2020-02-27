5e6882962c7877fa7444d981ec2cf3040205cbe2
Luke <luke@MacBook-Air.local>
2020-02-27 08:57:19 -0600

    repeatedly prompts user for input until valid response is provided

--- sort.c
+++ sort.c
@@ -15,17 +15,25 @@ int main()
     int manual_array[10]; // define array to be sorted manually
     int qsort_array[10];  // define array to be sorted with built in function
     int input; // holds user input
+    char line[256]; // holds line retrieved by fgets
+    int invalid_input = 1; // false only after valid input is read
+
+    while(invalid_input){
     printf("1) Generate Random Array\n");
     printf("2) Custom Array (user defined values)\n");
     printf("User Choice: ");
 
-    char line[256];
+    
     fgets(line, sizeof(line), stdin);
-    if( 1 == sscanf(line, "%d", &input)){
-        printf("You choose: %d \n", input);
+    if( 1 == sscanf(line, "%d", &input) && (input > 0 && input < 3)){
+        printf("You choose: %d \n", input); // only for testing
+        invalid_input = 0; // set to end while loop
+    }
+    else{
+        printf("Input must be 1 or 2!\n");
     }
     
-    
+    }
 
     int random;           // hold random number
     srand(time(NULL));    // sets up new sequence of pseudo-random numbers to be returned by rand()
